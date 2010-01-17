571d94c7e0f0a0c01423a880625b1e375d354e06
Torbjörn Lönnemark <tobbez@ryara.net>
2010-01-17 22:23:20 +0100

    add application logic

--- WhatUnavailableAlbums/MainForm.cs
+++ WhatUnavailableAlbums/MainForm.cs
@@ -16,6 +16,13 @@ namespace WhatUnavailableAlbums
             InitializeComponent();
         }
 
+        private void Log(string message)
+        {
+            LogBox.Text += "[" + DateTime.Now.ToString() + "] " + message + Environment.NewLine;
+            LogBox.SelectionStart = LogBox.Text.Length - 1;
+            LogBox.ScrollToCaret();
+        }
+
         private void BrowseButton_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
@@ -23,6 +30,50 @@ namespace WhatUnavailableAlbums
             {
                 MusicDirectory.Text = fbd.SelectedPath;
             }
+        }
+
+        private void ScanButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Log("Logging in...");
+                WhatHelper.Login(Username.Text, Password.Text);
+                
+
+                foreach (string artistPath in System.IO.Directory.GetDirectories(MusicDirectory.Text))
+                {
+                    foreach (string albumName in System.IO.Directory.GetDirectories(artistPath))
+                    {
+                        Application.DoEvents();
+
+                        Album album = new Album(System.IO.Path.GetFileName(artistPath), System.IO.Path.GetFileName(albumName));
+                        Log("Checking for " + album.ToString());
+                        if (!WhatHelper.AlbumIsAvailable(album))
+                            albumBindingSource.Add(album);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(ex.GetType().ToString() + ": " + ex.Message);
+            }
+        }
+
+        private void CopyListButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Album album in albumBindingSource)
+            {
+                sb.AppendLine(album.ToString());
+            }
+            Clipboard.SetText(sb.ToString());
+            Log("Album list copied to clipboard");
+        }
+
+        private void ClearListButton_Click(object sender, EventArgs e)
+        {
+            albumBindingSource.Clear();
+            Log("Album list cleared");
         }
     }
 }
